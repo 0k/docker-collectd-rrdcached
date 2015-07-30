4153bb55977a95162e72d385fcfc59bb12c8fa45
Valentin Lab <valentin.lab@kalysto.org>
2015-07-30 09:20:59 +0200

    new: force ``rrdcached`` in foreground mode to see the logs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p /var/lib/rrdcached/journal
-/usr/bin/rrdcached -l unix:/var/run/rrdcached.sock -j /var/lib/rrdcached/journal/ -F -b /var/lib/rrdcached/db/ -B -l 0.0.0.0 -p /var/run/rrdcached.pid &
+/usr/bin/rrdcached -l unix:/var/run/rrdcached.sock -j /var/lib/rrdcached/journal/ -F -b /var/lib/rrdcached/db/ -B -l 0.0.0.0 -p /var/run/rrdcached.pid -g &
 /usr/sbin/collectd -f "$@"
 
